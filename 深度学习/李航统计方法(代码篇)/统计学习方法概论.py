3d2c9d60d0b868fb8763cf5688ece511287eaeaa
金帆 <45331183+jinfanhahaha@users.noreply.github.com>
2019-07-16 10:35:58 +0800

    Update 统计学习方法概论.py

--- 深度学习/李航统计方法(代码篇)/统计学习方法概论.py
+++ 深度学习/李航统计方法(代码篇)/统计学习方法概论.py
@@ -22,10 +22,10 @@ def residuals_func(p, x, y):
     return ret
 # 书中提到  高斯于1823年在误差e1,…,en独立同分布的假定下,证明了最小二乘方法的一个最优性质:（证明略）
 # 在所有无偏的线性估计类中,最小二乘方法是其中方差最小的！ 对于数据(xi,yi)(i=1,2,3...,m)
-# 所以我们用最小二乘法去求出多项式的各个参数，比较好的是numpy给我们封装好了最小二乘法leastsq()
+# 所以我们用最小二乘法去求出多项式的各个参数，比较好的是scipy给我们封装好了最小二乘法leastsq()
 # leastsq() 简单介绍：
 # 它的参数很多，我们一般提取前三个参数 。func：误差函数 ，x0：表示函数的参数 ， args（）表示数据点
-
+from scipy.optimize import leastsq
 # 四、到此，我们可以自然的想到，写一个函数，它能够返回多项式的参数值，参数是经过最小二乘法最优的。
 def fitting(M=0): # 加M参数目的：人为的控制多项式项数。
     # 随机初始化多项式参数
@@ -55,7 +55,28 @@ def fitting(M=0): # 加M参数目的：人为的控制多项式项数。
    过拟合的现象，一般来说解决过拟合可以从以下几个方面入手：1、降低模型复杂度。2、正则化。3、增加样本数据。4、尝试化简，选择提取更好的特征。
    5、去噪。6、集成学习的方法。这里我们用正则化来实现降低模型复杂度的方法'''
 # 正则化：给模型的参数加上了一定的正则约束，比如将权值的大小加入到损失函数中。
-
+# 好，我们添加一个正则项（加上正则约束）
+regularization = 0.0001
+def residuals_func_regularization(p, x, y): 
+    ret = fit_func(p, x) - y
+    ret = np.append(ret,
+                    np.sqrt(0.5 * regularization * np.square(p))) # L2范数作为正则化项
+'''这是在原来的残差函数中，加上了L2正则化项后的函数，这一步的目的就是约束参数的大小，防止有些参数为了拟合数据而过大，导致过拟合。
+   具体想深入了解，可以查阅正则化相关的内容'''
+# 接下来就是将正则化封装进我们的函数里，并且实现可视化
+def fitting(M=0):
+    # 随机初始化多项式参数
+    p_init = np.random.rand(M + 1)
+    # 最小二乘法
+    p_lsq_regularization = leastsq(residuals_func_regularization, p_init, args=(x, y)) # 正则约束
+    p_lsq = leastsq(residuals_func, p_init, args=(x, y))                               # 常规
+    # 可视化
+    plt.plot(x_points, real_func(x_points), label='real')
+    plt.plot(x_points, fit_func(p_lsq[0], x_points), label='fitted curve')
+    plt.plot(x, y, 'bo', label='noise')
+    plt.plot(x_points,fit_func(p_lsq_regularization[0], x_points),label='regularization')
+    plt.legend()
+    return p_lsq
   
 
 
