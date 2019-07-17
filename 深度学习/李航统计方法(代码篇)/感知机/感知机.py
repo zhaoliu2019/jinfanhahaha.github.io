0f218e1daf1e55a558215ffc557d99f3e52d6894
金帆 <45331183+jinfanhahaha@users.noreply.github.com>
2019-07-17 08:20:31 +0800

    Update 感知机.py

--- 深度学习/李航统计方法(代码篇)/感知机/感知机.py
+++ 深度学习/李航统计方法(代码篇)/感知机/感知机.py
@@ -1,1 +1,12 @@
-
+'''理解感知机的思想后，如何用代码实现一个我们自己的感知机，网上有很多实现模型的框架，这里我们用scikit—learn框架模板来实现，框架如下：'''
+class X:
+  def __init__(self):
+    pass
+  def fit(self):
+    pass
+  def predict(self):
+    pass
+  def score(self):
+    pass
+  def __repr__(self):
+    pass
