33182b3e578f93d4414b015df38e4c3f7ac63c1d
金帆 <45331183+jinfanhahaha@users.noreply.github.com>
2019-07-17 08:54:45 +0800

    Update 感知机.py

--- 深度学习/李航统计方法(代码篇)/感知机/感知机.py
+++ 深度学习/李航统计方法(代码篇)/感知机/感知机.py
@@ -1,12 +1,27 @@
-'''理解感知机的思想后，如何用代码实现一个我们自己的感知机，网上有很多实现模型的框架，这里我们用scikit—learn框架模板来实现，框架如下：'''
+'''理解感知机的思想后，如何用代码实现一个我们自己的感知机，网上有很多实现模型的框架，这里我们用scikit—learn框架模板来实现。
+   模板如下：'''
+
 class X:
-  def __init__(self):
+  def __init__(self): # 初始化
     pass
-  def fit(self):
+  def fit(self):      # 训练
     pass
-  def predict(self):
+  def predict(self):  # 预测值
     pass
-  def score(self):
+  def score(self):    # 准确率
     pass
-  def __repr__(self):
-    pass
+  
+# 开始表演
+
+# 第一步 __init__方法，我们的感知机参数有系数向量w和截距b，而随机梯度下降有参数学习率l_rate，进行初始化
+def __init__(self):
+  self.w = None
+  self.b = None
+  self.l_rate = None
+  
+# 第二步，用随机梯度来训练我们的模型
+
+  
+
+  
+  
